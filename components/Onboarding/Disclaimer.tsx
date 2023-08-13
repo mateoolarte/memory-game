97c1dcf78edf83ae09d37206895087f3686517ce
Mateo Olarte <mateo.olarte8@gmail.com>
2023-08-12 23:10:06 -0500

    fix espaced word

--- components/Onboarding/Disclaimer.tsx
+++ components/Onboarding/Disclaimer.tsx
@@ -1,7 +1,7 @@
 export function Disclaimer() {
   return (
     <p className="text-slate-500 text-left text-sm mt-2">
-      Don't worry you only need to do this once
+      Don&apos;t worry you only need to do this once
     </p>
   );
 }
